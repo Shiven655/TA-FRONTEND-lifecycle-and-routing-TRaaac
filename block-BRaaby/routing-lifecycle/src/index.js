8ac5527d9aa7843c8dc7632bafe61ac17b99fe89
Shiven655 <kshiven655@gmail.com>
2023-06-17 23:29:21 +0530

    app modification remains

---
+++ block-BRaaby/routing-lifecycle/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/App.js';
+import './stylesheet/style.css';
+ReactDom.render(<App />, document.getElementById('root'));
